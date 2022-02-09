9f28f424b4e5bb62e034cfdd2f8a1401d2227412
jepp3999 <jeppe@surrow.com>
2022-02-09 15:50:58 +0100

    Made some exercises

---
+++ exercises/uge2/Basics/main.cs
@@ -0,0 +1,30 @@
+using System;
+using static System.Console;
+
+public static class test
+{
+  public static int n = 7;
+  public static double pi = 3.1415927;
+  public static double e = 2.71828;
+}
+
+public static class main {
+  //static string s = "main\n";
+  static void hello ()
+  {
+    string s = "hello\n";
+      Write (s);
+    {
+      string s1 = "block\n";
+        Write (s1);
+  }}
+static int Main() {
+    double x, y;
+      x = test.pi;
+      y = test.e;
+      Write ("x={0} y={1}\n", x, y);
+      Write ($"x={x} y={y}\n");
+	math.test();
+      return 0;
+  }
+}
---
+++ exercises/uge2/Basics/math.cs
@@ -0,0 +1,8 @@
+using static System.Console;
+using static System.Math;
+public static class math{
+	public static void test(){
+		double x=Sin(9),y=Cos(9);
+		WriteLine($"Sin^2+Cos^2={x*x+Pow(y,2)}");
+	}
+}
