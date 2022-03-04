bfd32344e265c17803fa781f511c1c7235a91371
jepp3999 <jeppe@surrow.com>
2022-03-04 15:02:47 +0100

    Did homework vec

---
+++ homework/vec/main.cs
@@ -0,0 +1,24 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class main{
+	static void Main(){
+		vec u=new vec(100,200,300);
+		u.print("u=");
+		vec v=new vec(1,2,3);
+		v.print("v=");
+		(u+v).print("u+v = ");
+		(5*v).print("5*v=");
+		var tmp=u*5;
+		tmp.print("u*5 = ");
+		var w=3*u-v;	
+		w.print("w=");
+		vec.print(w);
+		WriteLine(vec.approx(w,3*u-v));
+		WriteLine(w.approx(3*u-v));
+		WriteLine(u.approx(v));
+		WriteLine($"u.dot(v)={u.dot(v)}");
+}
+
+}
--- homework/vec/vec.cs
+++ homework/vec/vec.cs
@@ -4,7 +4,7 @@ using static System.Math;
 public class vec{
 	public double x,y,z;
 	//constructors:
-	public vec(){x=y=z=0}
+	public vec(){x=y=z=0;}
 	public vec(double a, double b, double c){
 		x=a;y=b;z=c;
 		}
@@ -19,8 +19,25 @@ public class vec{
 	//operators:
 	public static vec operator*(vec v, double c){return new vec(c*v.x,c*v.y,c*v.z);}
 	public static vec operator*(double c, vec v){return v*c;}
-	public static vec operator+(vec u, vec v){return new vec(u.x+v.x,u.y+v.y,u.z+v.z)}
-	public static vec operator+(vec u){return u}
-	public static vec operator-(vec u, vec v){return new vec(u.x-v.x,u.y-v.y,u.z-v.z)}
-	public static vec operator-(vec u){return -1*u}
+	public static vec operator+(vec u, vec v){return new vec(u.x+v.x,u.y+v.y,u.z+v.z);}
+	public static vec operator+(vec u){return u;}
+	public static vec operator-(vec u, vec v){return new vec(u.x-v.x,u.y-v.y,u.z-v.z);}
+	public static vec operator-(vec u){return -1*u;}
+
+public double dot(vec other){return this.x*other.x+this.y*other.y+this.z*other.z;}
+public static double dot(vec v, vec w){return v.x*w.x+v.y*w.y+v.z*w.z;}
+
+static bool approx(double a,double b,double tau=1e-9,double eps=1e-9){
+	if(Abs(a-b)<tau)return true;
+	if(Abs(a-b)/(Abs(a)+Abs(b))<eps)return true;
+	return false;
+	}
+public bool approx(vec other){
+	if(!approx(this.x,other.x))return false;
+	if(!approx(this.y,other.y))return false;
+	if(!approx(this.z,other.z))return false;
+	return true;
+	}
+public static bool approx(vec u, vec v){return u.approx(v); }
+
 }
