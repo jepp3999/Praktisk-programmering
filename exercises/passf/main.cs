499da3653baf221694a8e1b8acd17162144ff981
jepp3999 <jeppe@surrow.com>
2022-04-07 15:32:53 +0200

    Finished I/O

--- exercises/passf/main.cs
+++ exercises/passf/main.cs
@@ -3,6 +3,14 @@ using static System.Console;
 using static System.Math;
 using static table;
 
-class main{
-
-
+public static class main{
+	public static void Main(){
+	double k=1;
+	System.Func<double,double> sinkx = delegate(double x){return Sin(k*x);};
+	for(double i=1;i<=3;i++){
+	k=i;
+	Write($" k={k} sin(k*x)\n");
+	make_table(sinkx,0,PI,PI/10);
+	}
+	}
+}
