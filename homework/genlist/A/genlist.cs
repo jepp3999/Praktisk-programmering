649cc9e8c17c0aff7f91c9eb84fdfd66e7b4d22b
jepp3999 <jeppe@surrow.com>
2022-04-07 17:10:55 +0200

    Did genlist A

--- homework/genlist/A/genlist.cs
+++ homework/genlist/A/genlist.cs
@@ -6,7 +6,7 @@ public class genlist<T>{
 	public T[] data;
 	public int size {get{return data.Length;}} // property
 	public genlist(){ data = new T[0]; }
-	public void push(T item){ /* add item to the list */
+	public void push(T item){
 		T[] newdata = new T[size+1];
 		for(int i=0;i<size;i++)newdata[i]=data[i];
 		newdata[size]=item;
