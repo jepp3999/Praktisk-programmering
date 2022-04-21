bd5cbfd0cf7957f7fb79afe43053fecaa0acd911
jepp3999 <jeppe@surrow.com>
2022-04-21 12:03:07 +0200

    Finished plots B

--- homework/plots/B/main.cs
+++ homework/plots/B/main.cs
@@ -11,9 +11,18 @@ class main{
 	return Exp(lngamma);
 	}
 
+
+	static double lngamma(double x){
+	///single precision gamma function (Gergo Nemes, from Wikipedia)
+	if(x<0)return Double.NaN;
+	if(x<9)return lngamma(x+1)-Log(x);
+	double loggamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
+	return loggamma;
+	}
+
 	public static void Main(){
-		for(double x=0;x<=6;x+=1.0/10){
-			Write($"{x} {gamma(x)}\n");
+		for(double x=-4.95;x<=5;x+=1.0/10){
+			Write($"{x} {gamma(x)} {lngamma(x)}\n");
 		}
 	}
 }
