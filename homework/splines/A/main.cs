c08e78055bf10916829f8e582de13ac74d141abc
jepp3999 <jeppe@surrow.com>
2022-05-04 11:38:33 +0200

    Debugging spline A

--- homework/splines/A/main.cs
+++ homework/splines/A/main.cs
@@ -6,25 +6,23 @@ using static System.Math;
 
 class main{
 	public static void Main(){
-		int N=5;
-		int div=3;
-		double step = 1.0/div;
-		double[] xtable = new double[N*div];
-		double[] ytable = new double[N*div];
-		double[] yinteg = new double[N*div];
-		int k=0;
-		for(double i=0.0; i<N; i+=step){
-			xtable[k] = i;
-			ytable[k] = Cos(i);
-			yinteg[k] = Sin(i);
-			Write($"{xtable[k]} {ytable[k]} {yinteg[k]}\n");
-			k++;
+		double a=0,b=10;
+		int N=10;
+		double step = (b-a)/(N-1);
+		double[] xtable = new double[N];
+		double[] ytable = new double[N];
+		double[] yinteg = new double[N];
+		for(int i=0; i<N; i++){
+			xtable[i] = a+i*step;
+			ytable[i] = xtable[i];
+			yinteg[i] = Pow(xtable[i],2)/2;
+			Write($"{xtable[i]} {ytable[i]} {yinteg[i]}\n");
 		}
 		Write("\n\n"); //This is to create a new index in output file
-		for(double z=0.0; z<xtable[xtable.Length-1]; z+=step/2){
-		double linterp = linspline.linterp(xtable,ytable,z);
-		double linterpInteg = linspline.linterpInteg(xtable,ytable,z);
-		Write($"{z} {linterp} {linterpInteg}\n");
+		for(double z=0.0; z<xtable[xtable.Length-1]; z+=step/64){
+			double linterp = linspline.linterp(xtable,ytable,z);
+			double linterpInteg = linspline.linterpInteg(xtable,ytable,z);
+			Write($"{z} {linterp} {linterpInteg-1}\n");
 		}
 	}
 }
---
+++ homework/splines/B/qspline.cs
@@ -0,0 +1,10 @@
+using System;
+using Static System.Console;
+using Static System.Math;
+
+public class qspline{
+	qinterp(double[] x, double[] y, double z){
+		int i = binsearch(x,z);
+		p
+		double b[i] = 
+		s[i] = y[i]  + b[i]*(z-x[i]) + c[i]*(z-x[i])*(z-x[i])
