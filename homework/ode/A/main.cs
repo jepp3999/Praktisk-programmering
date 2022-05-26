091acf7dbebb1fe4e2b284125926c1b90440972e
jepp3999 <jeppe@surrow.com>
2022-05-26 14:21:22 +0200

    Fixed exercise odeint

---
+++ homework/ode/A/main.cs
@@ -0,0 +1,13 @@
+using System;
+using static System.Console;
+using static System.Math;
+using static ode;
+
+public class main{
+    public static void Main(){
+        double a = 0;
+        vector ya = new vector(0,1);
+        double b = 4*PI;
+    }
+}
+
---
+++ homework/ode/A/ode.cs
@@ -0,0 +1,40 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public class ode{
+    public static (vector,vector) rkstep12(
+	Func<double,vector,vector> f, /* the f from dy/dx=f(x,y) */
+	double x,   /* the current value of the variable */
+	vector y,   /* the current value y(x) of the sought function */
+	double h    /* the step to be taken */
+    ){ // Runge-Kutta Euler/Midpoint method (probably not the most effective)
+    	vector k0 = f(x,y); /* embedded lower order formula (Euler) */
+	    vector k1 = f(x+h/2,y+k0*(h/2)); /* higher order formula (midpoint) */
+    	vector yh = y+k1*h;     /* y(x+h) estimate */
+	    vector er = (k1-k0)*h;  /* error estimate */
+	    return (yh,er);
+    }
+
+    public static vector driver(
+    Func<double,vector,vector> f, /* the f from dy/dx=f(x,y) */
+   	double a,                     /* the start-point a */
+   	vector ya,                    /* y(a) */
+   	double b,                     /* the end-point of the integration */
+   	double h=0.01,                  /* initial step-size */
+   	double acc=0.01,              /* absolute accuracy goal */
+	double eps=0.01               /* relative accuracy goal */
+    ){
+        if(a>b) throw new Exception("driver: a>b");
+        double x=a; vector y=ya;
+        do{
+            if(x>=b) return y; /* job done */
+            if(x+h>b) h=b-x;   /* last step should end at b */
+            var (yh,erv) = rkstep12(F,x,y,h);
+            double tol = Max(acc,yh.norm()*eps) * Sqrt(h/(b-a));
+            double err = erv.norm();
+            if(err<=tol){ x+=h; y=yh; } // accept step
+        	h *= Min( Pow(tol/err,0.25)*0.95 , 2); // reajust stepsize
+        }while(true);
+    }//driver
+}
